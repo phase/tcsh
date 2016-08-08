d3b4013bd1771c16fe07c1284a9d08259e39bbf6
Jadon Fowler <jadonflower@gmail.com>
2016-08-08 00:39:22 -0700

    Initial Commit
    The source was taken from http://www.tcsh.org/MostRecentRelease
    
    I do not own this code, nor am I aware of the license.

---
+++ patchlevel.h
@@ -0,0 +1,14 @@
+/* $Header: /p/tcsh/cvsroot/tcsh/patchlevel.h,v 3.185 2015/05/21 21:38:12 christos Exp $ */
+/*
+ * patchlevel.h: Our life story.
+ */
+#ifndef _h_patchlevel
+#define _h_patchlevel
+
+#define ORIGIN "Astron"
+#define REV 6
+#define VERS 19
+#define PATCHLEVEL 0
+#define DATE "2015-05-21"
+
+#endif /* _h_patchlevel */
